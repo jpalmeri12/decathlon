9d9973a3347db0acf1867b6c6b446896a6318f3f
mint-cipher <cpumaster930@optonline.net>
2017-12-29 16:06:46 -0500

    Undefined catch

--- js/script.js
+++ js/script.js
@@ -2299,13 +2299,15 @@ function getHighScorePlace(score, highScores) {
 }
 
 function toOrdinal(n) {
-    if (n % 10 == 1 && n % 100 != 11) {
+    if (n == undefined) {
+        return "???";
+    } else if (n % 10 == 1 && n % 100 != 11) {
         return n + "st";
     } else if (n % 10 == 2 && n % 100 != 12) {
         return n + "nd";
     } else if (n % 10 == 3 && n % 100 != 13) {
         return n + "rd";
-    } else {
+    } else if (n > 0) {
         return n + "th";
     }
 }
